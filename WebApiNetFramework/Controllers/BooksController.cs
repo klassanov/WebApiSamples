66e681af75a01ef127327f81c43b66d572e2e150
Alexander Klassanov <klassanov@gmail.com>
2020-10-08 12:32:13 +0300

    Attribute routing tests

---
+++ WebApiNetFramework/Controllers/BooksController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApiNetFramework.Controllers
+{
+    [RoutePrefix("api/books")]
+    public class BooksController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "book1", "book2" };
+        }
+
+        [Route("{bookId:int}")]
+        public string Get(int bookId)
+        {
+            return $"book{bookId}";
+        }
+
+        [Route("first/{author}/{n}/getfirst")]
+        public string GetFirst(string author, int n)
+        {
+            return "GetFirstCalled";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
